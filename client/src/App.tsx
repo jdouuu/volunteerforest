269f572706524ed92ebc052198319cb12bb5c2ae
ddouuu <dou3@cougarnet.uh.edu>
2025-06-30 02:41:56 -0500

    front end react +
    front end only

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <h1>Welcome to Volunteer Forest</h1>
+    </div>
+  );
+}
+
+export default App;
