be4ca6c4787d99317dba1006e6b747e6d4dee883
ddouuu <dou3@cougarnet.uh.edu>
2025-06-30 03:36:09 -0500

    vite config to js

---
+++ client/src/App.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const jsx_runtime_1 = require("react/jsx-runtime");
+require("./index.css");
+function App() {
+    return ((0, jsx_runtime_1.jsx)("div", { className: "min-h-screen bg-gray-50", children: (0, jsx_runtime_1.jsx)("h1", { children: "Welcome to Volunteer Forest" }) }));
+}
+exports.default = App;
